d59ba0b335cd8a1a5d0914bc9ca30173c07edbeb
Alex Sanders <a@snd.rs>
2021-10-19 12:51:19 +0100

    lint

--- packages/simple-ts-test/src/index.ts
+++ packages/simple-ts-test/src/index.ts
@@ -1,28 +1,28 @@
 export interface Character {
-  name: string;
-  power: string;
-  universe?: string;
+	name: string;
+	power: string;
+	universe?: string;
 }
 
 export const spiderman: Character = {
-  name: "spiderman",
-  power: "spins a web like a spider can",
-  universe: "marvel",
+	name: 'spiderman',
+	power: 'spins a web like a spider can',
+	universe: 'marvel',
 };
 
 export const nhs: Character = {
-  name: "NHS",
-  power: "caring for and healing others",
+	name: 'NHS',
+	power: 'caring for and healing others',
 };
 
 export const superman: Character = {
-  name: "superman",
-  power: "strength",
-  universe: "dc",
+	name: 'superman',
+	power: 'strength',
+	universe: 'dc',
 };
 
 export const batman: Character = {
-  name: "batman",
-  power: "gadgets",
-  universe: "dc",
+	name: 'batman',
+	power: 'gadgets',
+	universe: 'dc',
 };
